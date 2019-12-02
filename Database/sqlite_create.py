0aa648f93c0f359955c75d161162118ee40a2504
Christine Demetillo <mgd22@njit.edu>
2019-12-01 23:07:47 -0500

    Feature: Step 2. Inserting Data

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -20,7 +20,10 @@ class Customer(Base):
     updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
     orders = relationship("Order", backref='customer')
 
-class Items(Base):
+    def __repr__(self):
+        return "<Customer:{0}-{1}>".format(self.id, self.username)
+
+class Item(Base):
     __tablename__ = 'items'
     id = Column(Integer(), primary_key=True)
     name = Column(String(200), nullable=False)
@@ -32,12 +35,18 @@ class Items(Base):
         CheckConstraint('quantity > 0', name='quantity_check'),
     )
 
+    def __repr__(self):
+        return "<Item:{0}-{1}>".format(self.id, self.name)
+
 class Order(Base):
     __tablename__ = 'orders'
     id = Column(Integer(), primary_key=True)
     customer_id = Column(Integer(), ForeignKey('customers.id'))
     date_placed = Column(DateTime(), default=datetime.now, nullable=False)
     date_shipped = Column(DateTime())
+
+    def __repr__(self):
+        return "<Order:{0}>".format(self.id)
 
 class OrderLine(Base):
     __tablename__ = 'order_lines'
@@ -48,10 +57,105 @@ class OrderLine(Base):
     order = relationship("Order", backref='order_lines')
     item = relationship("Item")
 
+    def __repr__(self):
+        return "<OrderLine:{0}>".format(self.id)
 Base.metadata.create_all(engine)
 
-#Step 1. Creating Session
+##Step 1. Creating Session
 session = Session(bind=engine)
+
+##Step 2. Inserting Data
+c1 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
+
+c2 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+
+session.add_all([c1, c2])
+session.commit()
+
+c3 = Customer(
+    first_name="John",
+    last_name="Lara",
+    username="johnlara",
+    email="johnlara@mail.com",
+    address="3073 Derek Drive",
+    town="Norfolk"
+)
+
+c4 = Customer(
+    first_name="Sarah",
+    last_name="Tomlin",
+    username="sarahtomlin",
+    email="sarahtomlin@mail.com",
+    address="3572 Poplar Avenue",
+    town="Norfolk"
+)
+
+c5 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
+
+c6 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+
+session.add_all([c3, c4, c5, c6])
+session.commit()
+
+i1 = Item(name='Chair', cost_price=9.21, selling_price=10.81, quantity=5)
+i2 = Item(name='Pen', cost_price=3.45, selling_price=4.51, quantity=3)
+i3 = Item(name='Headphone', cost_price=15.52, selling_price=16.81, quantity=50)
+i4 = Item(name='Travel Bag', cost_price=20.1, selling_price=24.21, quantity=50)
+i5 = Item(name='Keyboard', cost_price=20.1, selling_price=22.11, quantity=50)
+i6 = Item(name='Monitor', cost_price=200.14, selling_price=212.89, quantity=50)
+i7 = Item(name='Watch', cost_price=100.58, selling_price=104.41, quantity=50)
+i8 = Item(name='Water Bottle', cost_price=20.89, selling_price=25, quantity=50)
+
+session.add_all([i1, i2, i3, i4, i5, i6, i7, i8])
+session.commit()
+
+o1 = Order(customer=c1)
+o2 = Order(customer=c1)
+
+line_item1 = OrderLine(order=o1, item=i1, quantity=3)
+line_item2 = OrderLine(order=o1, item=i2, quantity=2)
+line_item3 = OrderLine(order=o2, item=i1, quantity=1)
+line_item3 = OrderLine(order=o2, item=i2, quantity=4)
+
+session.add_all([o1, o2])
+
+session.new
+session.commit()
+
+o3 = Order(customer=c1)
+orderline1 = OrderLine(item=i1, quantity=5)
+orderline2 = OrderLine(item=i2, quantity=10)
+
+o3.order_lines.append(orderline1)
+o3.order_lines.append(orderline2)
+
+session.add_all([o3])
+
+session.commit()
 
 #import sqlite3
 
