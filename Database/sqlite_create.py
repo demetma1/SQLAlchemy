bdebd6e8b1cb763755d3ca2d75a15801379b23a8
Christine Demetillo <mgd22@njit.edu>
2019-12-07 17:10:16 -0500

    Feature: Step 3 - BETWEEN

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -219,9 +219,10 @@ print(session.query(Order).filter(Order.date_shipped != None).all())
 print(session.query(Customer).filter(Customer.first_name.in_(['Toby', 'Sarah'])).all())
 
 #NOT IN
-print (session.query(Customer).filter(Customer.first_name.notin_(['Toby', 'Sarah'])).all())
+print(session.query(Customer).filter(Customer.first_name.notin_(['Toby', 'Sarah'])).all())
 
-
+#BETWEEN
+print(session.query(Item).filter(Item.cost_price.between(10, 50)).all())
 #import sqlite3
 
 #conn = sqlite3.connect('/web/Sqlite-Data/example.db')
