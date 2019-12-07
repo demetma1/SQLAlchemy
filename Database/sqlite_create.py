66a09085bd6815efaedf0c1242af72f14b36e024
Christine Demetillo <mgd22@njit.edu>
2019-12-07 17:58:26 -0500

    Feature: Step 3 -join() method

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -272,6 +272,21 @@ print(session.query(Item).filter(Item.name.ilike("wa%")).order_by(desc(Item.cost
 #conn.commit()
 #conn.close()
 
+#join() method
+print(session.query(Customer).join(Order))
+session.query(Table1).join(Table2).join(Table3).join(Table4).all()
+
+print(session.query(
+    Customer.first_name,
+    Item.name,
+    Item.selling_price,
+    OrderLine.quantity
+).join(Order).join(OrderLine).join(Item).filter(
+    Customer.first_name == 'John',
+    Customer.last_name == 'Green',
+    Order.id == 1,
+).all())
+
 
 
 
